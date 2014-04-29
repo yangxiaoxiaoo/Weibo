98ed0f1fb3404c6efeea6411940d9d2a1ba8829b
Xiaofeng <yangxiaoxiaoo@163.com>
2014-04-29 16:09:00 -0400

    LDA added

---
+++ LDA_preprocessing.py
@@ -0,0 +1,31 @@
+import logging, gensim, bz2
+import os
+from gensim import corpora, models, similarities
+
+#load dictionary
+id2word = gensim.corpora.Dictionary.load("../temp_corpus_dict")
+
+documents_folder = "../UserTokenized/"
+#corpus
+cop_wlist = []
+for user_doc in os.listdir(documents_folder):
+    #documents
+    doc_wlist = []
+    user_name = user_doc.split('_')[0]
+    file_dir = os.path.join(documents_folder, user_doc)
+    user_file = open(file_dir, 'r')
+    for line in user_file:
+        for word in line.split(' '):
+            doc_wlist.append(word)
+    cop_wlist.append(doc_wlist)
+    doc_vector = id2word.doc2bow(doc_wlist)
+    print doc_vector
+
+corpus = [id2word.doc2bow(doc_text) for doc_text in cop_wlist]
+corpora.MmCorpus.serialize('../Weibo_corpus.mm',corpus)
+print "------------------------------corpus--------------------------------------"
+print(corpus)
+
+mm = gensim.corpora.MmCorpus('../Weibo_corpus.mm')
+print mm
+#mm is the corpus iterator, it should contain documents
